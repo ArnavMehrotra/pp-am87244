97f4d7b94bfde8849c147cf71ba3215a103c7f83
Arnav Mehrotra <arnavmehrotra@Arnavs-MacBook-Pro.local>
2021-09-12 12:38:57 -0500

    added java and fixed simple script

---
+++ java/code/Table.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+
+public class Table {
+
+    public static void main(String[] args) {
+        for(int i = 0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+        read_table();
+        
+    }
+
+    private static int[][] read_table(){
+        return null;
+    }
+}
