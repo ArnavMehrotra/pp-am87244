235605fb3eac7eeed484663518f125d3d5d6b4a9
Arnav Mehrotra <arnavmehrotra@Arnavs-MacBook-Pro.local>
2021-09-15 20:44:43 -0500

    parsing and print mostly work

--- java/code/Table.java
+++ java/code/Table.java
@@ -1,23 +1,76 @@
 import java.lang.reflect.Array;
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
     //TODO: ERROR HANDLING
+    private ArrayList<ArrayList<String>> matrix;
+    private String outFile;
+    private boolean header;
+
+    public Table(String tableFileString, String outFile, boolean header){
+        this.header = header;
+        this.matrix = new ArrayList<>();
+        this.outFile = outFile;
+        try{
+            BufferedReader tableReader = new BufferedReader(new FileReader(Paths.get(tableFileString).toFile()));
+            if(header){
+                tableReader.readLine();
+            }
+            String line = tableReader.readLine();
+            int j = 0;
+            while(line != null){
+                String[] tokens = line.split("\\s");
+                matrix.add(new ArrayList<String>());
+                for(int i = 0; i<tokens.length; i++){
+                    matrix.get(j).add(tokens[i]);
+                }
+                line = tableReader.readLine();
+                j++;
+            }
+            tableReader.close();
+        } catch(IOException i){
+            //TODO:: handle exception
+        }
+
+    }
     public static void main(String[] args) {
-        parse_args(args);
+        if(args.length == 0){
+            //TODO: figure out the actual error
+            System.out.println("please provide arguments");
+            System.exit(-1);
+        }
+        launch_args(args);
     }
 
-    private static void parse_args(String[] args){
+    private static void launch_args(String[] args){
         boolean header = false;
-        if(args[0].strip().equals("-header")){
+        if(args[0].equals("-header")){
             header = true;
         }
         int cmdIndex = header ? 1 : 0;
         String cmd = args[cmdIndex];
         switch(cmd){
             case "-print":{
-                //TODO: print
+                //TODO: ERROR HANDLING
+                String[] colsStrings = args[cmdIndex+1].split(",");
+                String tableFileString = args[cmdIndex+2];
+                String outFileString = args[cmdIndex+3];
+
+                //read columns
+                int[] cols = new int[colsStrings.length];
+                for(int i = 0; i<colsStrings.length; i++){
+                    try {
+                        Integer.parseInt(colsStrings[i]);
+                    } catch (Exception e) {
+                        System.out.println("COL INDEX ERROR");
+                    }
+                }
+                
+                Table table = new Table(tableFileString, outFileString, header);
+                table.printCols(cols);
                 break;
             }
             case "-sum":{
@@ -39,9 +92,10 @@ public class Table {
             default:{
                 try {
                     FileInputStream fileStream = new FileInputStream(Paths.get(cmd).toFile());
-                    String cmd_strings[] = (String[]) fileStream.toString().lines().toArray();
+                    String cmd_string = (String) fileStream.toString();
+                    String cmd_strings[] = cmd_string.split("\\s+");
                     for(int i = 0; i<cmd_strings.length; i++){
-                        parse_args(cmd_strings[i].split("\\s+"));
+                        launch_args(cmd_strings[i].split("\\s+"));
                     }
                 } catch (Exception e) {
                     //TODO: handle exception
@@ -50,9 +104,28 @@ public class Table {
                 break;
             }
         }
+
     }
 
-    private static int[][] read_table(){
-        return null;
+    private void printCols(int[] cols){
+        try{
+            FileOutputStream outFileStream = new FileOutputStream(Paths.get(outFile).toFile());
+            System.out.println(matrix);
+            for(int i = 0; i<matrix.get(0).size(); i++){
+                for(int j = 0; j<cols.length; j++){
+                    try {
+                        outFileStream.write(matrix.get(i).get(j).getBytes()); 
+                    } catch (IndexOutOfBoundsException a) {
+                        System.out.println("COL INDEX ERROR");
+                    }
+                    outFileStream.write("\n".getBytes());
+                }
+            }
+            outFileStream.flush();
+        } catch(Exception e){
+            //TODO: handle exception
+            System.exit(-1);
+        }
+        
     }
 }
