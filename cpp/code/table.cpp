916f69f2524ffd8299efeddd9353d486420251e0
Arnav Mehrotra <arnavmehrotra@wireless-10-145-162-175.public.utexas.edu>
2021-09-28 15:58:01 -0500

    add getline and stuff

--- cpp/code/table.cpp
+++ cpp/code/table.cpp
@@ -1,4 +1,45 @@
 
-int main() {
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+typedef std::string String;
+
+class TableEntry{
+
+};
+
+class Table{
+    private:
+        TableEntry** values;
+        String outFile;
+        int n_rows;
+        int n_cols;
+        String* header;
+    public:
+    
+    Table(String tableFile, String out, bool head){
+        outFile = out;
+        std::ifstream t(tableFile);
+        String fileString;
+        std::getline(t, fileString);
+        std::cout << fileString << "\n";
+    }
+
+};
+
+int main(int argc, char* argv[]){
+    if(argc < 4){
+        std::cout << "OTHER ERROR\n";
+        exit(-1);
+    }
+    bool header = strcmp(argv[1], "-header") == 0;
+    String cmdString = String(argv[header + 1]);
+
+    if(cmdString.compare("-print") == 0){
+        Table* t = new Table(argv[header+3], argv[header+4], header);
+    }
     return 0;
 }
