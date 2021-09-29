b9f19b1d90b62b2932e1667dedab3b7f6c91f818
ArnavMehrotra <arnavm22@yahoo.com>
2021-09-28 22:48:41 -0500

    print is underway

--- cpp/code/table.cpp
+++ cpp/code/table.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <sstream>
 
-typedef std::string String;
+using namespace std;
 
 typedef enum TYPE{
     STR,
@@ -13,46 +13,134 @@ typedef enum TYPE{
 }_type;
 
 typedef union _value{
-    char* str;
+    string str;
     double num;
 }Value;
 
 class TableEntry{
-    TYPE type;
-    Value val;
+    public:
+        TYPE type;
+        Value val;
 };
 
 class Table{
     private:
         TableEntry** values;
-        String outFile;
-        int n_rows;
-        int n_cols;
-        String* header;
+        string outFile;
+        int rows;
+        int cols;
+        string* header;
     public:
-    
-    Table(String tableFile, String out, bool head){
-        outFile = out;
-        std::ifstream t(tableFile);
-        String fileString;
-        std::getline(t, fileString);
-        while(!fileString.empty()){
-            std::cout << fileString << "\n";
-            std::getline(t, fileString);
+        Table(string tableFile, string out, bool head){
+            outFile = out;
+            string buffer[256][256];
+
+            ifstream t(tableFile);
+            string fileString;
+            getline(t, fileString);
+            int r = 0;
+            int c = 0;
+
+            //read file into buffer
+            while(!fileString.empty()){
+                stringstream ss(fileString);
+                string val;
+                int last_c = c;
+                c = 0;
+                while(ss >> val){
+                    buffer[r][c] = val; 
+                    c++; //oops I did it again 
+                }
+                if((last_c != c) && (last_c != 0)){
+                    cout << "NUM COLS ERROR\n";
+                    exit(-1);
+                }
+                getline(t, fileString);
+                r++;
+            }
+
+            if(head){
+                header = (string*) calloc(c, sizeof(string));
+                for(int i = 0; i<c; i++){
+                    header[i] = buffer[0][i];
+                }
+            }
+        
+            values = (TableEntry**) calloc(r, sizeof(TableEntry*));
+            for(int i = 0; i<r; i++){
+                values[i] = (TableEntry*)calloc(c, sizeof(TableEntry));
+            }
+
+            for(int i=0; i<r-head; i++){
+                for(int j=0; j<c; j++){
+                    if((atoi(buffer[i+head][j].c_str()) == 0) && (buffer[i+head][j].c_str()[0] != '0')){
+                        values[i][j].type = STR;
+                        values[i][j].val.str = buffer[i+head][j];
+                    }
+                    else{
+                        values[i][j].type = NUM;
+                        values[i][j].val.num = atof(buffer[i+head][j].c_str());
+                    }
+                }
+            }
+
+            rows = r-head;
+            cols = c;
         }
-    }
+
+        void printCols(int n_cols, int printCols[]){
+            for(int i = 0; i<n_cols; i++){
+                if((printCols[i] >= cols) || printCols[i] < 0){
+                    cout << "COL INDEX ERROR\n";
+                }
+            }
+
+            if(header != NULL){
+                for(int i = 0; i<n_cols; i++){
+                    cout << header[i] << " ";
+                }
+                cout << "\n";
+            }
+
+            for(int i = 0; i<rows; i++){
+                for(int j = 0; j<n_cols; j++){
+                    if(values[i][printCols[j]].type == NUM){
+                        printf("%.5g ",values[i][printCols[j]].val.num);
+                    }
+                    else{
+                        cout << values[i][printCols[j]].val.str << " ";
+                    }
+                }
+                cout << "\n";
+            }
+        }
 
 };
 
 int main(int argc, char* argv[]){
     if(argc < 5){
-        std::cout << "OTHER ERROR\n";
+        cout << "OTHER ERROR\n";
         exit(-1);
     }
     bool header = strcmp(argv[1], "-header") == 0;
-    String cmdString = String(argv[header + 1]);
+    string cmdString = string(argv[header + 1]);
 
     if(cmdString.compare("-print") == 0){
+        int cols[256];
+        int n_cols = 0;
+
+        char* colNum = strtok(argv[header+2], ",");
+        while(colNum != NULL){
+            cols[n_cols] = atoi(colNum);
+            n_cols++;
+            if((cols[n_cols] == 0) && (strcmp(colNum, "0") != 0)){
+                cout << "COL INDEX ERROR\n";
+                exit(-1);
+            }
+        }
+
+
+
         Table* t = new Table(argv[header+3], argv[header+4], header);
     }
     return 0;
